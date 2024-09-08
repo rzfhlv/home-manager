7360bc429aa18d32ec73a821e028a0a8ae1f0bf3
Reza Fahlevi <rzfhlv.me@gmail.com>
2024-09-08 03:11:50 +0000

    Reza Fahlevi

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Home Manager configuration of tercyduck";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, nixpkgs-stable, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      stable-pkgs = nixpkgs-stable.legacyPackages.${system};
+    in {
+      homeConfigurations."tercyduck" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+	extraSpecialArgs = {
+	  inherit stable-pkgs;
+	};
+      };
+    };
+}
